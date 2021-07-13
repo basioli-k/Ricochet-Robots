28ce4d092f0cc0875fb4ce7f88f469e63037eb39
basioli-k <k.basioli@gmail.com>
2021-07-13 20:33:57 +0200

    skoro baza

--- app/updateBaze.php
+++ app/updateBaze.php
@@ -9,15 +9,32 @@ $rezultati_string = file_get_contents($filename1);
 $rezultati = explode(',', $rezultati_string);
 
 $username_string = file_get_contents($filename2);
-$usernames = explode( ',',$username)
+$usernames = explode( ',', $username_string);
+$najbolji_rez = 0;
 
 for ($i = 0; $i < count($rezultati) - 1; $i++) {
     $ime = explode(':', $rezultati[$i])[0];
-    $broj = explode(':', $rezultati[$i])[1];
-    $polje[$i] = array($ime, $broj);
+    $rezultat = explode(':', $rezultati[$i])[1];
+    $polje[$i] = [ "ime" => $ime, "rezultat" => (int) $rezultat, "najbolji" => false ];
+    if ((int) $rezultat > $najbolji_rez)
+        $najbolji_rez = (int) $rezultat;
 }
 
-print_r($usernames);
+for ($i = 0 ; $i < count($usernames) - 1; $i++){
+    $found = false;
+    for($j = 0; $j < count($polje) ; $j++){
+        if ( $polje[$j]["ime"] == $usernames[$i] ){
+            $found = true;
+        }
+    }
+    if (!$found)
+        array_push($polje, ["ime" => $usernames[$i], "rezultat" => 0, "najbolji" => false]);
+}
+
+for ($i = 0 ; $i < count($polje) ; $i++){
+    if ($polje[$i]["rezultat"] === $najbolji_rez)
+        $polje[$i]["najbolji"] = true;
+}
 
 print_r($polje);
 
